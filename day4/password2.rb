7469b4a7c3ed19a4a019c98335a691f6cc00450a
Timothy Nordloh <tnordloh@gmail.com>
2019-12-05 19:58:05 -0700

    fix password2

--- day4/password2.rb
+++ day4/password2.rb
@@ -1,7 +1,7 @@
 def valid?(password)
   pairs = password.to_s.chars[0..-2].zip(password.to_s.chars[1..-1])
   pairs.all? { |pair| pair.first <= pair.last } &&
-    pairs.select { |pair| pair.first == pair.last }.size == 1
+    pairs.one? { |pair| pair.first == pair.last }
 end
 
 p (272_091..815_432).select { |i| valid?(i) }.size
