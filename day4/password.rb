4d76d213e5bdd42ece77618eb929c0aac8be0a09
Timothy Nordloh <tnordloh@gmail.com>
2019-12-05 19:48:45 -0700

    day 4 part 1

---
+++ day4/password.rb
@@ -0,0 +1,7 @@
+def valid?(password)
+  pairs = password.to_s.chars[0..-2].zip(password.to_s.chars[1..-1])
+  pairs.all? { |pair| pair.first <= pair.last } &&
+    pairs.any? { |pair| pair.first == pair.last }
+end
+
+p (272_091..815_432).select { |i| valid?(i) }.size
